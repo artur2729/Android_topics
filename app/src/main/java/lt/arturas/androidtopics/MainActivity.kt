2304d76031c233e90feceb9cc47fe402a0f3af96
Artur <a.tymulis@gmail.com>
2023-05-09 16:10:17 +0300

    fixed, now opens second activity

--- app/src/main/java/lt/arturas/androidtopics/MainActivity.kt
+++ app/src/main/java/lt/arturas/androidtopics/MainActivity.kt
@@ -19,13 +19,17 @@ class MainActivity : ActivityLifecycles() {
         openSecondActivityButton = findViewById(R.id.button)
         itemListView = findViewById(R.id.itemListView)
 
+        val items = mutableListOf<Item>()
+        generateListOfItems(items)
+        
+        setupListView()
+        updateAdapter(items)
+
+        setClickOpenItemDetails()
+
         setClickOpenSecondActivity()
 
 
-        val items = mutableListOf<Item>()
-        generateListOfItems(items)
-
-        adapter = CustomAdapter(this)
         adapter.add(items)
         adapter.add(Item(101, "text01","text02"))
         adapter.add(
@@ -34,16 +38,35 @@ class MainActivity : ActivityLifecycles() {
             Item(104, "text01","text02"),
             Item(105, "text01","text02")
         )
+        
+    }
 
+    private fun setClickOpenItemDetails() {
+        itemListView.setOnItemClickListener { adapterView, view, position, l ->
+            val item: Item = adapterView.getItemAtPosition(position) as Item
 
-
-        itemListView.adapter = adapter
-
-        /*
-        for(item in items){
-            Timber.i(item.toString())
+            val itemIntent = Intent(this, SecondActivity2::class.java)
+            itemIntent.putExtra(MAIN_ACTIVITY_ITEM_ID,item.id)
+            itemIntent.putExtra(MAIN_ACTIVITY_ITEM_TEXT01,item.text01)
+            itemIntent.putExtra(MAIN_ACTIVITY_ITEM_TEXT02,item.text02)
+            startActivity(itemIntent)
         }
-         */
+    }
+
+    private fun updateAdapter(items: MutableList<Item>) {
+        adapter.add(items)
+        adapter.add(Item(101, "text01","text02"))
+        adapter.add(
+            Item(102, "text01","text02"),
+            Item(103, "text01","text02"),
+            Item(104, "text01","text02"),
+            Item(105, "text01","text02")
+        )
+    }
+
+    private fun setupListView() {
+        adapter = CustomAdapter(this)
+        itemListView.adapter = adapter
     }
 
     private fun generateListOfItems(items: MutableList<Item>) {
@@ -62,5 +85,11 @@ class MainActivity : ActivityLifecycles() {
         openSecondActivityButton.setOnClickListener {
             startActivity(Intent(this, SecondActivity2::class.java))
         }
+    }
+
+    companion object{
+        const val MAIN_ACTIVITY_ITEM_ID = "lt.arturas.androidtopics_item_id"
+        const val MAIN_ACTIVITY_ITEM_TEXT01 = "lt.arturas.androidtopics_item_text01"
+        const val MAIN_ACTIVITY_ITEM_TEXT02 = "lt.arturas.androidtopics_item_text02"
     }
 }
--- app/src/main/java/lt/arturas/androidtopics/SecondActivity2.kt
+++ app/src/main/java/lt/arturas/androidtopics/SecondActivity2.kt
@@ -8,7 +8,10 @@ class SecondActivity2 : ActivityLifecycles() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second2)
-        timber("onCreate")
+        setContentView(R.layout.activity_second)
+
+        val id = intent.getIntExtra(MainActivity.MAIN_ACTIVITY_ITEM_ID, -1)
+        val text01 = intent.getStringExtra(MainActivity.MAIN_ACTIVITY_ITEM_TEXT01)
+        val text02 = intent.getStringExtra(MainActivity.MAIN_ACTIVITY_ITEM_TEXT02)
     }
 }
