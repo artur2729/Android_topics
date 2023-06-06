cd8ed9a51a7dbfba35498fc8d8def3e65669f76b
Artur <a.tymulis@gmail.com>
2023-06-06 16:08:13 +0300

    Add 2 fragments

--- app/src/main/java/lt/arturas/androidtopics/common/MainActivity.kt
+++ app/src/main/java/lt/arturas/androidtopics/common/MainActivity.kt
@@ -1,12 +1,23 @@
 package lt.arturas.androidtopics.common
 
 import android.os.Bundle
+import androidx.fragment.app.commit
 import lt.arturas.androidtopics.R
+import lt.arturas.androidtopics.first_fragment.FirstFragment
 
 //ctrl alt l o
 class MainActivity : ActivityLifecyclesPresentation() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace(
+                R.id.fragmentContainerView,
+                FirstFragment.newInstance(),
+                "first_fragment"
+            )
+            setReorderingAllowed(true)
+        }
     }
 }
