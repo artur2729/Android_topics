7ee38c3c1a2e0c864c0042eb7e80f7207c9d9a83
Artur <a.tymulis@gmail.com>
2023-06-13 14:04:06 +0300

    Preparation for navigation graph topic

--- app/src/main/java/lt/arturas/androidtopics/first_fragment/FirstFragment.kt
+++ app/src/main/java/lt/arturas/androidtopics/first_fragment/FirstFragment.kt
@@ -7,20 +7,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import lt.arturas.androidtopics.R
+import lt.arturas.androidtopics.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
     private val viewModel: FirstFragmentViewModel by viewModels()
 
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
         fun newInstance() = FirstFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
--- app/src/main/java/lt/arturas/androidtopics/second_fragment/SecondFragment.kt
+++ app/src/main/java/lt/arturas/androidtopics/second_fragment/SecondFragment.kt
@@ -7,19 +7,30 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import lt.arturas.androidtopics.R
+import lt.arturas.androidtopics.databinding.FragmentFirstBinding
+import lt.arturas.androidtopics.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
 
     private val viewModel: SecondFragmentViewModel by viewModels()
 
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_second, container, false)
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
         fun newInstance() = SecondFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
